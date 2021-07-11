3f8f6863eb14d01a738b9c33e52ce672acdfba3b
devomohamed <mohammedsabry633@gmail.com>
2021-07-11 10:06:54 +0200

    add all files

---
+++ js/test.js
@@ -0,0 +1,55 @@
+// owl carousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+new WOW().init();
+$(document).ready(function(){
+    $(".loading").remove();
+    $("body").css("overflow" , "auto");
+
+
+    // Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  $("header .header-content-end .secound span").text(seconds);
+  $("header .header-content-end .munit span").text(minutes); 
+  $("header .header-content-end .hour span").text(hours); 
+  $("header .header-content-end .days span").text(days); 
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+});
+
+
